522cb63984109eb1554e47e0cfde243e4ffead62
fuxiaohei <fuxiaohei@vip.qq.com>
2018-08-28 15:42:16 +0800

    [new] 添加 center-2.5.3的 tag

--- apps/agent/build.go
+++ apps/agent/build.go
@@ -2,5 +2,5 @@ package main
 
 const (
     // BuildTime is auto generated build time
-    BuildTime = "2018-08-26T13:49:00+0800"
+    BuildTime = "2018-08-28T15:19:03+0800"
 )
--- apps/center/build.go
+++ apps/center/build.go
@@ -2,5 +2,5 @@ package main
 
 const (
     // BuildTime is auto generated build time
-    BuildTime = "2018-08-26T13:49:42+0800"
+    BuildTime = "2018-08-28T15:20:33+0800"
 )
